b11633e51f6d88bda1ea4a4386be686856a3ec8d
Luiz Cavalieri <luiz@interactivehippo.com.au>
2018-03-03 16:05:42 +1100

    commenting modules imports groups

--- src/app/shared-module/shared-module.module.ts
+++ src/app/shared-module/shared-module.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+// Extra Modules.
 import {
   MatButtonModule,
   MatCardModule
 } from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+// Custom created modules.
 
 @NgModule({
   imports: [
