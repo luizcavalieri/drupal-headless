d8924533ea1f156e68aed6441398c5cbb417b483
Luiz Cavalieri <luiz@interactivehippo.com.au>
2018-03-12 21:06:33 +1100

    testing team city and circle ci.

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,14 @@
+// export const configs = {
+//   API_BASE_URL: 'http://headless-drupal.local/api/',
+//   BE_BASE_URL: 'http://headless-drupal.local/'
+// };
+//
+// export const configs = {
+//   API_BASE_URL: 'http://headless-drupal.local/api/',
+//   BE_BASE_URL: 'http://headless-drupal.local/'
+// };
+
 export const configs = {
-  API_BASE_URL: 'http://headless-drupal.local/api/',
-  BE_BASE_URL: 'http://headless-drupal.local/'
+  API_BASE_URL: 'http://headless-drupal.local:8081/api/',
+  BE_BASE_URL: 'http://headless-drupal.local:8081/'
 };
